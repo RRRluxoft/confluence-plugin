9a1f2712c3f5f2e8d3d9f670cec100b6505072f0
RRRluxoft <rrr.lands@gmail.com>
2019-08-16 11:21:24 +0200

    confluence plugin - added; renamed to jira-plugin

--- jira-plugin/src/test/java/ut/com/sixdx/confluence/plugin/MyComponentUnitTest.java
+++ jira-plugin/src/test/java/ut/com/sixdx/confluence/plugin/MyComponentUnitTest.java
@@ -1,8 +1,8 @@
 package ut.com.sixdx.confluence.plugin;
 
+import org.junit.Test;
 import com.sixdx.confluence.plugin.api.MyPluginComponent;
 import com.sixdx.confluence.plugin.impl.MyPluginComponentImpl;
-import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
